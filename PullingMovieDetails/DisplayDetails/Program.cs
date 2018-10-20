2a21857d76c3af7cd63d493b537603ab67009ddd
Alrick Visagie <alrick@firstword.co>
2018-10-20 19:41:56 +0200

    initial commit

---
+++ PullingMovieDetails/DisplayDetails/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace DisplayDetails
+{
+    using System;
+    using System.Collections.Generic;
+    using MovieApi;
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ////Details of single movies
+            //singleMovieDisplay();
+
+            //Details of multiple movies
+            multipleMovieDisplay();
+
+            Console.ReadKey();
+        }
+
+        public static void singleMovieDisplay()
+        {
+            Movie movie = new Movie();
+            RetrieveMovie getMovie = new RetrieveMovie();
+
+            movie = getMovie.getMovie("Deadpool");
+
+            Console.WriteLine("---------------------------------------------------------------------------------------------------");
+            Console.WriteLine("Name =   " + movie.Title + " \n" +
+                                "Caption =   " + movie.Plot + " \n" +
+                                "Image =   " + movie.Poster + " \n" +
+                                "Trailer =   " + movie.TrailerUrl);
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+        }
+
+        public static void multipleMovieDisplay()
+        {
+            RetrieveMovie getMovie = new RetrieveMovie();
+            List<Movie> movies = new List<Movie>();
+
+            movies = getMovie.getMovies(new List<string> { "Deadpool", "Safe", "Safe House" });
+            foreach (var movie in movies)
+            {
+                Console.WriteLine("---------------------------------------------------------------------------------------------------");
+                Console.WriteLine("Name =   " + movie.Title + " \n" +
+                                    "Caption =   " + movie.Plot + " \n" +
+                                    "Image =   " + movie.Poster + " \n" +
+                                    "Trailer =   " + movie.TrailerUrl);
+                Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+            }
+        }
+    }
+}
---
+++ PullingMovieDetails/MovieApi/RetrieveMovie.cs
@@ -0,0 +1,45 @@
+﻿namespace MovieApi
+{
+    using System.Collections.Generic;
+    using System.Net;
+    using Newtonsoft.Json;
+    public class RetrieveMovie
+    {
+        public Movie  getMovie(string movieName)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            Movie movie = new Movie();
+            string url1 = "http://www.omdbapi.com/?t=" + movieName + "&apikey=867d7138";
+            using (WebClient wc = new WebClient())
+            {
+                var json = wc.DownloadString(url1);               
+                movie = JsonConvert.DeserializeObject<Movie>(json);
+                if (movie.Response == "True")
+                {
+                    return movie;
+                }
+            }
+            return null;
+        }
+        public List<Movie>  getMovies(List<string> movieName)
+        {
+            List<Movie> movies = new List<Movie>();
+            JsonSerializer serializer = new JsonSerializer();
+            Movie movie = new Movie();
+            foreach (var name in movieName)
+            {
+                string url1 = "http://www.omdbapi.com/?t=" + name + "&apikey=867d7138";
+                using (WebClient wc = new WebClient())
+                {
+                    var json = wc.DownloadString(url1);
+                    movie = JsonConvert.DeserializeObject<Movie>(json);
+                    if (movie.Response == "True")
+                    {
+                        movies.Add(movie);
+                    }
+                }
+            }
+            return movies;
+        }
+    }
+}
